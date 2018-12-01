d3c65a3f460001344b2b57937bf5332e7f49b4dc
micky387 <mickaelsaibi@free.fr>
2018-12-01 22:10:47 +0100

    (temp) Add permissive mod for now

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -24,6 +24,7 @@ BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(DEVICE_PATH)/bluetooth/include
 
 # Kernel
 TARGET_KERNEL_CONFIG := sdm845-perf_defconfig
+BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 
 # inherit from the proprietary version
 -include vendor/oneplus/fajita/BoardConfigVendor.mk
